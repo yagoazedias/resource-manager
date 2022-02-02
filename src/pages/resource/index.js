f920a28cc93e8a1059e217640300e10fd019aaf5
yagoazedias <yago.azedias@nubank.com.br>
2022-02-02 12:57:27 -0300

    Adding recurso form

--- src/pages/resource/index.js
+++ src/pages/resource/index.js
@@ -1,65 +1,80 @@
 import React, { useEffect, useState } from 'react';
 import { Layout, Center } from 'atoms';
 import { Navbar } from 'organisms';
-import { Formik } from 'formik';
-import { Routes, Route, useParams } from "react-router-dom";
+import { Form, Button } from 'react-bootstrap';
+import { useParams } from "react-router-dom";
 
 import API from 'api';
 
 const Resource = () => {
-  const [resource, setResource] = useState([])
+
   const params = useParams();
+  const [editable, setEditable] = useState(false)
+  const [resource, setResource] = useState([])
 
   useEffect(() => {
     async function fetchData() {
       const response = await API.getResourceById(params.id)
       setResource(response)
+      console.log(response.titulo)
     }
     fetchData();
   }, [])
 
-  const onSubmit = values => {
-    console.log(values)
+  const getAttributeBykey = (resource, key) => {
+    if (resource[key]) {
+      return resource[key]
+    }
+    return "Carregando..."
+  }
+
+  const getDateInputFromIso = (isoformat) => {
+
+    if(isoformat === "Carregando...") return "Carregando..."
+
+    const date = new Date(isoformat);
+    return date.toISOString().substring(0, 10);
   }
   
   return (
       <Center width="1500px">
         <Navbar/>
         <Layout>
-        <Formik
-          onSubmit={onSubmit}
-          initialValues={{
-            titulo: "",
-            descricao: "",
-            data_de_criacao: "",
-            data_de_registro: "",
-            colecao_id: {
-                "id": 94,
-                titulo: "Os eventos do ano de 2026 nova"
-            }
-        }}
-        >
-          {({
-            values,
-            handleSubmit,
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                name="text"
-                value={values.titulo}
-              />
-              <input
-                type="text"
-                name="text"
-                value={values.titulo}
-              />
-              <button type="submit">
-                Submit
-              </button>
-            </form>
-          )}
-        </Formik>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Titulo</Form.Label>
+            <Form.Control value={getAttributeBykey(resource, "titulo")} disabled={!editable} type="email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Descricao</Form.Label>
+            <Form.Control value={getAttributeBykey(resource, "descricao")} as="textarea" rows={3} disabled={!editable} type="text"  />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Link</Form.Label>
+            <Form.Control value={getAttributeBykey(resource, "link")} rows={3} disabled={!editable} type="text"  />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Imagem (link)</Form.Label>
+            <Form.Control value={getAttributeBykey(resource, "imagem")} rows={3} disabled={!editable} type="text"  />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Data de criação</Form.Label>
+            <Form.Control value={getDateInputFromIso(getAttributeBykey(resource, "data_de_criacao"))} disabled={!editable} pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" rows={3} type="date"  />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Data de registro</Form.Label>
+            <Form.Control value={getDateInputFromIso(getAttributeBykey(resource, "data_de_registro"))} disabled={!editable} pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" rows={3} type="date"  />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
         </Layout>
       </Center>
   )
